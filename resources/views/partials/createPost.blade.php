652e98d635812b32578e038e4a4af4d96db574c0
nkoriath <koriathsemail@gmail.com>
2016-04-02 14:13:51 -0700

    adds max character length to the feed create

--- resources/views/partials/createPost.blade.php
+++ resources/views/partials/createPost.blade.php
@@ -1,8 +1,8 @@
 
-{!! Form::label('body', 'Body') !!}
-{!! Form::textarea('body', $post->message, ['class' => 'form-control', 'id' => 'message']) !!}
+{!! Form::label('message', 'Message') !!}
+{!! Form::text('message', $post->message, ['class' => 'form-control', 'id' => 'message', 'maxlength' => '250']) !!}
 
 {!! Form::label('tag', 'Tag') !!}
-{!! Form::text('tag', $post->tag, ['class' => 'form-control', 'id' => 'message']) !!}
+{!! Form::text('tag', $post->tag, ['class' => 'form-control', 'id' => 'tag']) !!}
 
 {!! Form::submit('Submit', array('class' => 'btn btn-primary')) !!}
