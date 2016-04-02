cf1fa3ecc2090c88eca7ae3ed288a9ca6f02efaa
nkoriath <koriathsemail@gmail.com>
2016-04-02 12:08:08 -0700

    adds scaffolding for user post creation

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -8,5 +8,20 @@ use App\Http\Requests;
 
 class feed extends Controller
 {
-    //
+    public function show(){
+
+    }
+    public function edit(){
+
+    }
+    //Better naming would be a good thing here
+    public function make(){
+
+    }
+    public function delete(){
+
+    }
+    public function create(){
+
+    }
 }
